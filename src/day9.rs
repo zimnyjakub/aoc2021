f013f3c31d6cdbcecca4f322315c24c24ce998b4
Jakub Zimny <zimny.jot@gmail.com>
2021-12-10 23:14:25 +0100

    d9

--- src/day9.rs
+++ src/day9.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 pub fn day9() {
     let string = fs::read_to_string("lavalamp.txt").unwrap();
-    let lines= string.lines();
+    let lines = string.lines();
 
     let rows: Vec<&str> = lines.collect();
 
@@ -12,18 +12,39 @@ pub fn day9() {
     for y in 0..rows.len() {
         let xes = rows[y].chars();
         for (x, value) in xes.enumerate() {
-            height_map.insert((x as i32,y as i32), value.to_string().parse().unwrap());
+            height_map.insert((x as i32, y as i32), value.to_string().parse().unwrap());
         }
     }
 
-    for p in &height_map {
-        println!("{:?}", p);
+    let mut risk_levels: BTreeMap<(i32, i32), i32> = BTreeMap::new();
+    for ((x, y), v) in &height_map {
+        let up = height_map.get(&(*x, *y - 1));
+        let down = height_map.get(&(*x, *y + 1));
+        let right = height_map.get(&(*x + 1, *y));
+        let left = height_map.get(&(*x - 1, *y));
+
+        if up.is_some() && up.unwrap() <= v {
+            continue;
+        }
+        if down.is_some() && down.unwrap() <= v {
+            continue;
+        }
+        if left.is_some() && left.unwrap() <= v {
+            continue;
+        }
+        if right.is_some() && right.unwrap() <= v {
+            continue;
+        }
+
+        risk_levels.insert((*x, *y), *v+1);
     }
 
-    let mut low_points: Vec<(i32,i32,i32)> = Vec::new();
-    for p in &height_map {
-        println!("{:?}", p);
+    for p in &risk_levels {
+        println!("{:?}", p)
     }
 
+    let aoc: i32 = risk_levels.iter().map(|it| it.1).sum();
+
+    println!("aoc ans: {}", aoc);
 }
 
